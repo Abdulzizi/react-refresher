c2f967efa0580138185fb77b0f9780ce4814ca21
Abdul Jawad Azizi <jawadazizi052@gmail.com>
2025-05-13 10:31:04 +0700

    lift up the state to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
 import PostList from "./components/PostList/PostList";
+import MainHeader from "./components/MainHeader/MainHeader";
+
+import { useState } from "react";
 
 function App() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const toggleModalHandler = () => {
+    setIsModalOpen((prevState) => {
+      if (!prevState) {
+        return !prevState;
+      }
+
+      resetStateHandler();
+      return !prevState;
+    });
+  };
+
+  const resetStateHandler = () => {
+    setBody('');
+    setAuthor('');
+  };
+
   return (
-    <main>
-      <PostList />
-    </main>
+    <>
+      <MainHeader onCreatePost={toggleModalHandler} />
+      <main>
+        <PostList
+          isPosting={isModalOpen}
+          onStopPosting={toggleModalHandler}
+          body={body}
+          author={author}
+          setBody={setBody}
+          setAuthor={setAuthor}
+        />
+      </main>
+    </>
   );
 }
 
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -4,11 +4,7 @@ import Post from "../Post/Post";
 import classes from "./PostList.module.css";
 import Modal from "../Modal/Modal";
 
-function PostList() {
-    const [body, setBody] = useState('');
-    const [author, setAuthor] = useState('');
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
+function PostList({ isPosting, onStopPosting, body, author, setBody, setAuthor }) {
     const changeAuthorHandler = (event) => {
         setAuthor(event.target.value);
     };
@@ -19,30 +15,13 @@ function PostList() {
         // console.log('Updated body:', event.target.value);
     };
 
-    const toggleModalHandler = () => {
-        setIsModalOpen((prevState) => {
-            if (!prevState) {
-                return !prevState;
-            }
-
-            resetStateHandler();
-            return !prevState;
-        });
-    };
-
-    const resetStateHandler = () => {
-        setBody('');
-        setAuthor('');
-    };
-
     return (
         <>
-            {isModalOpen && (
-                <Modal onClose={toggleModalHandler}>
-                    <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} onCancel={toggleModalHandler} />
+            {isPosting && (
+                <Modal onClose={onStopPosting}>
+                    <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler} onCancel={onStopPosting} />
                 </Modal>
             )}
-            <button onClick={toggleModalHandler}>New Post</button>
             <ul className={classes.postList}>
                 <li>
                     <Post title="Post 1" body={body} author={author} />
